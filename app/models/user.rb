32381e1c36c87f23472214450b6bd61b7d3c1306
makotopin <makotopin1016@gmail.com>
2022-04-01 16:21:54 +0900

    バリデーションの微調整とRuboCopによる整備

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, allow_blank: true }
+
   validates :nick_name, presence: true
+
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
   validates :birthday, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, } do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
-  end
-
 end
