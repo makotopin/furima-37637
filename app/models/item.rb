2de9f29537b0667c1a97ce41a514f7d170070ab9
makotopin <makotopin1016@gmail.com>
2022-04-02 01:14:17 +0900

    プルダウンボタンの実装とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :region
+  belongs_to :delivery_days
+
+  validates :name, :explanation, :price, presence: true
+  validates :category, :status, :shipping_cost, :region, :delivery_days, numericality: {other_than: 1 , message: "can't be blank"}
+
 end
