ba74b5a4bda99009f07858671708da44c7af9755
makotopin <makotopin1016@gmail.com>
2022-04-01 17:21:58 +0900

    ファクトリーボット内の調整

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :user do
     nick_name { Faker::Name.name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password { password }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name { '田中' }
     first_name { 'メアリー' }
