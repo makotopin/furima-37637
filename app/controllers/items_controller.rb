a848c5b67be45605178306e81157e958c8db554e
makotopin <makotopin1016@gmail.com>
2022-04-02 01:08:36 +0900

    出品ページへの遷移機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def deleate
+  end
+
+  private
+
+  
 end
