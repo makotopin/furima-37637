a936573ee53c3b6bafdab1cb3d205d9550a89c56
makotopin <makotopin1016@gmail.com>
2022-04-08 13:06:24 +0900

    アソシエーションとバリデーションの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :purchase_record
+  
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true}
+    validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipality, :block_number
+    validates :phone_number, format: {with: /\A[0-9]{,11}\z/, allow_blank: true}
+  end
+
 end
--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,2 +1,5 @@
 class PurchaseRecord < ApplicationRecord
+  belongs_to :user
+  belongs_to :Item
+  has_one :Address
 end
