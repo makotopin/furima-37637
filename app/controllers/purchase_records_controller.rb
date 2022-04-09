084f7870b5a83c6fe516d19e0f2c74d7fe53887b
makotopin <makotopin1016@gmail.com>
2022-04-09 15:10:20 +0900

    Formオブジェクトパターンに置き換え

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,22 +1,24 @@
 class PurchaseRecordsController < ApplicationController
 def index
   @item = Item.find(params[:item_id])
+  @purchase_record_address = PurchaseRecordAddress.new
 end
 
 def create
-  @purchase_record = PurchaseRecord.create!(purchase_record_params)
-  Address.create(address_params)
-  redirect_to root_path
+  @item = Item.find(params[:item_id])
+  @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
+  if @purchase_record_address.valid?
+    @purchase_record_address.save
+    redirect_to root_path
+  else
+    render :index
+  end
 end
 
 private
 
 def purchase_record_params
-  params.permit.merge(user_id: current_user.id, item_id: params[:item_id])
-end
-
-def address_params
-  params.permit(:post_code, :region_id, :municipality, :block_number, :building, :phone_number).merge(purchase_record_id: @purchase_record.id)
+  params.require(:purchase_record_address).permit(:post_code, :region_id, :municipality, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], purchase_record_id: params[:purchase_record_id])
 end
 
 end
