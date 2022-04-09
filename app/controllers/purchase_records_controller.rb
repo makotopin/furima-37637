bf033bf4aa089bb4ab2b6df35e7529f6e3a8abd7
makotopin <makotopin1016@gmail.com>
2022-04-09 12:26:08 +0900

    Formオブジェクトを使用せずにデータを保存する部分まで実装
    カード情報に関しては非表示で後回しにしています

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -4,7 +4,19 @@ def index
 end
 
 def create
-  
+  @purchase_record = PurchaseRecord.create!(purchase_record_params)
+  Address.create(address_params)
+  redirect_to root_path
+end
+
+private
+
+def purchase_record_params
+  params.permit.merge(user_id: current_user.id, item_id: params[:item_id])
+end
+
+def address_params
+  params.permit(:post_code, :region_id, :municipality, :block_number, :building, :phone_number).merge(purchase_record_id: @purchase_record.id)
 end
 
 end
--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,5 +1,5 @@
 class PurchaseRecord < ApplicationRecord
   belongs_to :user
-  belongs_to :Item
-  has_one :Address
+  belongs_to :item
+  has_one :address
 end
