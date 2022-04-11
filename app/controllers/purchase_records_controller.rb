ee94827416500d06767c36c0cd856ae24e42fdce
makotopin <makotopin1016@gmail.com>
2022-04-11 13:40:50 +0900

    決済機能のサーバーサイド実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,24 +1,33 @@
 class PurchaseRecordsController < ApplicationController
-def index
-  @item = Item.find(params[:item_id])
-  @purchase_record_address = PurchaseRecordAddress.new
-end
-
-def create
-  @item = Item.find(params[:item_id])
-  @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
-  if @purchase_record_address.valid?
-    @purchase_record_address.save
-    redirect_to root_path
-  else
-    render :index
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_record_address = PurchaseRecordAddress.new
   end
-end
 
-private
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
+    if @purchase_record_address.valid?
+      pay_item
+      @purchase_record_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
-def purchase_record_params
-  params.require(:purchase_record_address).permit(:post_code, :region_id, :municipality, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], purchase_record_id: params[:purchase_record_id])
-end
+  private
 
+  def purchase_record_params
+    params.require(:purchase_record_address).permit(:post_code, :region_id, :municipality, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], purchase_record_id: params[:purchase_record_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_41b0d69a582d7655e3a7ebb7"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_record_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,13 +1,13 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :region_id, :municipality, :block_number, :building, :phone_number, :purchase_record_id
+  attr_accessor :user_id, :item_id, :post_code, :region_id, :municipality, :block_number, :building, :phone_number, :purchase_record_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true}
     validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality, :block_number
     validates :phone_number, format: {with: /\A[0-9]{,11}\z/, allow_blank: true}
-    validates :user_id, :item_id
+    validates :user_id, :item_id, :token
   end
 
   def save
